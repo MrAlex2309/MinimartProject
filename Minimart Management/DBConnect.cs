b1a9d8838a3c9c6487f768f3510db76b32c511ad
MrAlex2309 <bensas797@gamil.com>
2023-03-08 16:33:59 +0700

    ADD SAMPLE

---
+++ Minimart Management/DBConnect.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Runtime.Remoting.Contexts;
+
+namespace Minimart_Management
+{
+    class DBConnect
+    {
+        private static SqlConnection conn;
+        private static object locker = new object();
+        static DBConnect instance;
+        protected DBConnect()
+        {
+            conn = new SqlConnection("Data Source=DESKTOP-Q3L9KAK;Initial Catalog=MiniMart;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+        public static DBConnect getInstance()
+        {
+            if(instance == null)
+            {
+                lock (locker)
+                {
+                    if(instance == null)
+                    {
+                        instance = new DBConnect();
+                    }
+                }
+            }
+            return instance;
+        }
+        public SqlConnection Getcon()
+        {
+            return conn;
+        }
+        public void Opencon()
+        {
+            if (conn.State == System.Data.ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+        }
+        public void Closecon()
+        {
+            if (conn.State == System.Data.ConnectionState.Open)
+            {
+                conn.Close();
+            }
+        }
+    }
+}
